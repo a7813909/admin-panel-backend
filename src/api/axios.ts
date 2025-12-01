dfda198249f8c4554b4d0c9b71f2e9df4e5b7177
a7813909 <a7813909@gmail.com>
2025-12-01 10:10:23 +0300

    fix: Use process.env for API_BASE_URL in backend axios client and update tsconfig

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-    const API_BASE_URL = import.meta.env.VITE_BASE_API_URL;
-
+    //const API_BASE_URL = import.meta.env.VITE_BASE_API_URL;
+const API_BASE_URL = process.env.API_BASE_URL
     // После наших мучений, эта проверка стала очень важной!
     if (!API_BASE_URL) {
       console.error("❌ Ошибка: Переменная окружения VITE_BASE_API_URL не определена. Проверьте .env файл и перезапустите сервер Vite.");
